c1dbdefd1c01f861613ef1efdcfaf98a55574d02
hohuynhnhu <hhnhu2003@gmail.com>
2025-12-02 23:37:29 +0700

    add thu vien

--- lib/views/home/widgets/AI_section.dart
+++ lib/views/home/widgets/AI_section.dart
@@ -1,7 +1,8 @@
 import 'package:barbergofe/core/theme/text_styles.dart';
 import 'package:barbergofe/views/home/widgets/AIConsultationItem.dart';
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
+import 'package:barbergofe/routes/route_names.dart';
 class AiSection extends StatelessWidget {
   const AiSection({super.key});
 
@@ -10,20 +11,33 @@ class AiSection extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('AI tu van', style: AppTextStyles.heading,),
-        const SizedBox(height: 12,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Aiconsultationitem(icon: Icons.face,label:  'AI tạo tóc'),
-              const SizedBox(width: 30,),
-              Aiconsultationitem(icon: Icons.medical_services_outlined, label: 'AI trị mụn'),
-              const SizedBox(width: 30,),
-              Aiconsultationitem(icon: Icons.chat_bubble_outline, label: 'AI chat'),
+        const Text('AI tư vấn', style: AppTextStyles.heading),
+        const SizedBox(height: 12),
+        Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 30,
+          children: [
+            Aiconsultationitem(
+              icon: Icons.face,
+              label: 'AI tạo tóc',
+            ),
+            Aiconsultationitem(
+              icon: Icons.medical_services_outlined,
+              label: 'AI trị mụn',
+              onTap: () {
+                print("Nút AI trị mụn được bấm");
+                context.goNamed('acne');
+              },
+            ),
 
-            ]
-        )
+            Aiconsultationitem(
+              icon: Icons.chat_bubble_outline,
+              label: 'AI chat',
+            ),
+          ],
+        ),
       ],
     );
   }
 }
+
