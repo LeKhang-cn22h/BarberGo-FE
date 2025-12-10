182ed60a3297cebf558bc45bcd4a21c9415cce0a
LeKhang-cn22h <chube02082017@gmail.com>
2025-12-10 22:39:39 +0700

    feat: add BarberEndpoint

---
+++ lib/api/endpoints/barber_endpoint.dart
@@ -0,0 +1,13 @@
+class BarberEndpoint{
+  // GET /barbers/{id} - Lấy thông tin barber theo ID
+  static const String barberGetById = "/barbers";
+
+  /// GET /barbers/areas - Lấy danh sách khu vực có barber
+  static const String barberGetAreas = "/barbers/areas";
+
+  /// GET /barbers/area - Lấy barbers theo khu vực
+  static const String barberGetByArea = "/barbers/area";
+
+  /// GET /barbers/top - Lấy danh sách top barbers
+  static const String barberGetTop = "/barbers/top";
+}
